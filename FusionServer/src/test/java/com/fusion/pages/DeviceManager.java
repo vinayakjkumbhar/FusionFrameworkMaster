7be5a9d7a01cbeb1be552f7f4be2a01cefa6bf12
Vinayak Kumbhar <vinayaksuk@gmail.com>
2022-07-31 12:36:18 +0530

    test

--- FusionServer/src/test/java/com/fusion/pages/DeviceManager.java
+++ FusionServer/src/test/java/com/fusion/pages/DeviceManager.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.WebDriver;
 public class DeviceManager {
 	
 	WebDriver driver;
-
+ int i=0;
 	By username=By.id("UserName");
 	By password=By.xpath("//input[@id='Password']");
 	By loginbutton=By.xpath("//input[@id='btnLogin']");
